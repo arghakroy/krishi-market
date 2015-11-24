24d5a1a7a288d8ef10056082e8218b8f6834e40d
ARGHA KAMOL ROY <argha@polluxlab.com>
2015-11-25 01:28:46 +0600

    Initialize

---
+++ app/src/main/java/me/argha/sustproject/LoginActivity.java
@@ -0,0 +1,26 @@
+package me.argha.sustproject;
+
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Bind(R.id.loginInputEmail) EditText mEmailEt;
+    @Bind(R.id.loginInputEmailLayout)TextInputLayout mEmailEtLayout;
+    @Bind(R.id.loginInputPassword) EditText mPasswordEt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+        ButterKnife.bind(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
