1fe2771fa14983136207fd968fffeaa891419606
ARGHA KAMOL ROY <argha@polluxlab.com>
2015-11-26 22:25:18 +0600

    Updated httphelper

--- app/src/main/java/me/argha/sustproject/utils/AppConst.java
+++ app/src/main/java/me/argha/sustproject/utils/AppConst.java
@@ -2,5 +2,7 @@ package me.argha.sustproject.utils;
 
 public class AppConst {
     public static final String APP_PREF = "sust_pref";
+    public static final int MAX_CONNECTION_TRY = 5;
+    public static final int TIME_OUT = 20000;
     public static String DEBUG_KEY="Lobster";
 }
