1bb141a5827a64ddcdb088e4fd8580b38c8bc11f
ARGHA KAMOL ROY <argha@polluxlab.com>
2015-11-27 13:52:30 +0600

    Added register fields

--- app/src/main/java/me/argha/sustproject/RegisterActivity.java
+++ app/src/main/java/me/argha/sustproject/RegisterActivity.java
@@ -1,13 +1,12 @@
 package me.argha.sustproject;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -18,10 +17,13 @@ import butterknife.ButterKnife;
  */
 public class RegisterActivity extends AppCompatActivity {
 
-    @Bind(R.id.registerNameEt) EditText name;
-    @Bind(R.id.registerEmailEt) EditText emailEt;
+    @Bind(R.id.registerNameEt) EditText fullNameEt;
+    @Bind(R.id.registerUserNameEt) EditText userNameEt;
     @Bind(R.id.registerPasswordEt) EditText passwordEt;
     @Bind(R.id.registerPasswordConfirmEt) EditText passwordConfirmEt;
+    @Bind(R.id.registerPhoneEt) EditText phoneEt;
+    @Bind(R.id.registerAddressEt) EditText addressEt;
+    @Bind(R.id.registerDistrictSpinner)Spinner districtSpinner;
     @Bind(R.id.registerSubmitBtn) Button submitButton;
     Context context;
 
@@ -33,14 +35,5 @@ public class RegisterActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         Toolbar toolbar= (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //start new activity. Change from LoginActivity
-                Intent intent= new Intent(context,LoginActivity.class);
-                startActivity(intent);
-
-            }
-        });
     }
 }
