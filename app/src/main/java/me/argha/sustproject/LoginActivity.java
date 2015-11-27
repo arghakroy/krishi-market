5d38555ba8230c3f368bb764f0dd9e604aba94d9
ARGHA KAMOL ROY <argha@polluxlab.com>
2015-11-27 14:36:41 +0600

    added http request for login and register

--- app/src/main/java/me/argha/sustproject/LoginActivity.java
+++ app/src/main/java/me/argha/sustproject/LoginActivity.java
@@ -1,5 +1,6 @@
 package me.argha.sustproject;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.support.design.widget.TextInputLayout;
@@ -10,17 +11,29 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONObject;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import cz.msebera.android.httpclient.Header;
+import me.argha.sustproject.helpers.HTTPHelper;
+import me.argha.sustproject.utils.AppURL;
+import me.argha.sustproject.utils.Util;
 
 public class LoginActivity extends AppCompatActivity {
 
 
     Context context;
+    AsyncHttpClient httpClient;
     @Bind(R.id.loginUserNameEt) EditText mEmailEt;
     @Bind(R.id.loginInputEmailLayout)TextInputLayout mEmailEtLayout;
     @Bind(R.id.loginPasswordEt) EditText mPasswordEt;
     @Bind(R.id.loginRegisterBtn) Button registerBtn;
+    @Bind(R.id.loginSubmitBtn) Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +43,52 @@ public class LoginActivity extends AppCompatActivity {
         context=this;
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        httpClient= HTTPHelper.getHTTPClient();
 
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(context,RegisterActivity.class));
+                startActivity(new Intent(context, RegisterActivity.class));
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginUser(mEmailEt.getText().toString(),mPasswordEt.getText().toString());
             }
         });
     }
 
+    private void loginUser(String username, String password) {
+        RequestParams params=new RequestParams();
+        params.add("username",username);
+        params.add("password", password);
+        final ProgressDialog dialog= Util.getProgressDialog(context,"Please wait...");
+        httpClient.post(AppURL.LOGIN,params,new JsonHttpResponseHandler(){
+            @Override
+            public void onStart() {
+                super.onStart();
+                dialog.show();
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+                Util.printDebug("Login response",response.toString());
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+                Util.printDebug("Login fail",responseString);
+            }
+
+            @Override
+            public void onFinish() {
+                super.onFinish();
+                dialog.cancel();
+            }
+        });
+    }
 }
--- app/src/main/java/me/argha/sustproject/RegisterActivity.java
+++ app/src/main/java/me/argha/sustproject/RegisterActivity.java
@@ -1,15 +1,27 @@
 package me.argha.sustproject;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONObject;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import cz.msebera.android.httpclient.Header;
+import me.argha.sustproject.helpers.HTTPHelper;
+import me.argha.sustproject.utils.AppURL;
+import me.argha.sustproject.utils.Util;
 
 /**
  * Author: ARGHA K ROY
@@ -35,5 +47,56 @@ public class RegisterActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         Toolbar toolbar= (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerUser(fullNameEt.getText().toString(),
+                    userNameEt.getText().toString(),
+                    passwordEt.getText().toString(),
+                    phoneEt.getText().toString(),
+                    addressEt.getText().toString(),
+                    districtSpinner.getSelectedItem().toString());
+            }
+        });
+    }
+
+    private void registerUser(String fullName, String userName, String password, String phone, String address, String district) {
+        RequestParams params=new RequestParams();
+        params.add("name",fullName);
+        params.add("username",userName);
+        params.add("password",password);
+        params.add("phone",phone);
+        params.add("address",address);
+        params.add("district",district);
+
+        final ProgressDialog dialog= Util.getProgressDialog(this,"Registering. Please wait...");
+        AsyncHttpClient httpClient= HTTPHelper.getHTTPClient();
+        httpClient.post(AppURL.REGISTER,params,new JsonHttpResponseHandler(){
+            @Override
+            public void onStart() {
+                super.onStart();
+                dialog.show();
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+                Util.printDebug("Register response",response.toString());
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+                Util.printDebug("Register fail",responseString);
+            }
+
+            @Override
+            public void onFinish() {
+                super.onFinish();
+                dialog.cancel();
+            }
+
+        });
     }
 }
