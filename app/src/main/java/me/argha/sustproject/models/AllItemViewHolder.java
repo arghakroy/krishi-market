0e4eaf95625ed1767baea76084590aef046bcca0
ARGHA KAMOL ROY <argha@polluxlab.com>
2015-11-28 02:52:13 +0600

    Updated View holder and layouts

--- app/src/main/java/me/argha/sustproject/models/AllItemViewHolder.java
+++ app/src/main/java/me/argha/sustproject/models/AllItemViewHolder.java
@@ -2,17 +2,27 @@ package me.argha.sustproject.models;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import me.argha.sustproject.R;
 
 /**
  * Author: ARGHA K ROY
  * Date: 11/27/2015.
  */
 public class AllItemViewHolder extends RecyclerView.ViewHolder {
-    protected String itemName;
-    protected String itemDistrict;
-    protected String itemRating;
-    protected String itemPriceRange;
+    public TextView itemName;
+    public TextView itemCategory;
+    public TextView itemRating;
+    public TextView itemPriceRange;
+    public ImageView itemImage;
     public AllItemViewHolder(View itemView) {
         super(itemView);
+        itemName= (TextView) itemView.findViewById(R.id.listSingleItemName);
+        itemCategory= (TextView) itemView.findViewById(R.id.listSingleItemCategory);
+        itemRating= (TextView) itemView.findViewById(R.id.listSingleItemRating);
+        itemPriceRange= (TextView) itemView.findViewById(R.id.listSingleItemPriceRange);
+        itemImage= (ImageView) itemView.findViewById(R.id.listItemImageIv);
     }
 }
